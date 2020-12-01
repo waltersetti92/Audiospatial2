aa5aedc61f7a7e5dadbf7fd146ef4766a9d74fd6
Walter Setti <walter.setti@iit.it>
2020-12-01 12:16:26 +0100

    Writings

--- Audiospatial/Finale_Scenario.Designer.cs
+++ Audiospatial/Finale_Scenario.Designer.cs
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labIndicazioni1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labIndicazioni1
+            // 
+            this.labIndicazioni1.AutoSize = true;
+            this.labIndicazioni1.BackColor = System.Drawing.Color.Transparent;
+            this.labIndicazioni1.Font = new System.Drawing.Font("Stencil", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labIndicazioni1.ForeColor = System.Drawing.Color.Black;
+            this.labIndicazioni1.Location = new System.Drawing.Point(3, 44);
+            this.labIndicazioni1.Name = "labIndicazioni1";
+            this.labIndicazioni1.Size = new System.Drawing.Size(652, 42);
+            this.labIndicazioni1.TabIndex = 16;
+            this.labIndicazioni1.Text = "LA MISSIONE E\' STATA COMPLETATA!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Stencil", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(3, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(670, 42);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "QUESTA MISSIONE UN PO\' SPECIALE...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Stencil", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(3, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(320, 42);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "...ERA UN SOGNO!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Stencil", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(3, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(798, 42);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "MA DA QUESTO SOGNO, AVUTO DA BAMBINO...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Stencil", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(3, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1097, 42);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "HO CAPITO DI VOLER DEDICARE LA MIA VITA ALLA MISSIONE!!!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Snap ITC", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Indigo;
+            this.label5.Location = new System.Drawing.Point(288, 596);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(712, 51);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "OTTIMO LAVORO DAVVERO!";
             // 
             // Finale_Scenario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Audiospatial.Properties.Resources.wakeup;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labIndicazioni1);
             this.Name = "Finale_Scenario";
-            this.Size = new System.Drawing.Size(738, 410);
+            this.Size = new System.Drawing.Size(873, 792);
             this.Load += new System.EventHandler(this.Finale_Scenario_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labIndicazioni1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
